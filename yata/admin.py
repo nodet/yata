3bd5cdf3d2b7342df48308c859592428e245dcdb
Xavier Nodet <xavier.nodet@gmail.com>
2011-01-22 18:33:15 +0000

    Show last_edited in admin view

--- yata/admin.py
+++ yata/admin.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('description', 'relative_due_date')
+    list_display = ('description', 'relative_due_date', 'last_edited')
     list_filter = ('description', 'due_date')
     
 admin.site.register(Task, TaskAdmin)
