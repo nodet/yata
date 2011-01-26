9c9e890754b10da1e02eb8b3862c9c292d031016
Xavier Nodet <xavier.nodet@gmail.com>
2011-01-26 16:54:00 +0000

    Moving the templates into the xno subdirectory

--- settings.py
+++ settings.py
@@ -81,7 +81,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    'f:/users/nodet/My Dropbox/src/python/django_templates'
+    'f:/users/nodet/My Dropbox/src/python/xno/public/templates'
 )
 
 INSTALLED_APPS = (
