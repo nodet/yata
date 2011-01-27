de030d53281a35fed043eaf235b4904cff88f0a2
Xavier Nodet <xavier.nodet@gmail.com>
2011-01-27 10:02:27 +0000

    Use javascript and JQuery for due date

--- urls.py
+++ urls.py
@@ -17,4 +17,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
+    
+    (r'^static_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'f:/users/nodet/My Dropbox/src/python/xno/public/media/', 'show_indexes': True }),    
 )
