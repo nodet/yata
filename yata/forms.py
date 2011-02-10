894d6bf64a5a0dee5faa4d9b50dc08cfd873ada3
Xavier Nodet <xavier.nodet@gmail.com>
2011-02-10 10:41:40 +0000

    Enlarge description and note fields, set focus on description or title

--- yata/forms.py
+++ yata/forms.py
@@ -5,11 +5,13 @@ from yata.models import Task, Context
 class AddTaskForm(forms.ModelForm):
     class Meta:
         model = Task
-    description = forms.CharField(widget=forms.TextInput(attrs={'size':'100'}))
+    description = forms.CharField(widget=forms.TextInput(attrs={'size':'100', 'id':'focused'}))
+    note        = forms.CharField(widget=forms.Textarea(attrs={'cols':'81', 'rows':'10'}))
         
 class AddContextForm(forms.ModelForm):
     class Meta:
         model = Context
+    title = forms.CharField(widget=forms.TextInput(attrs={'size':'50', 'id':'focused'}))
         
 class UploadXMLForm(forms.Form):
     file  = forms.FileField()
