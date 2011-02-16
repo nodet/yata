4166ef71bffda45184a163af296cf353a141d41e
Xavier Nodet <xavier.nodet@gmail.com>
2011-02-16 10:20:59 +0000

    Use a prefix for our views in URLConf

--- urls.py
+++ urls.py
@@ -5,34 +5,35 @@ import settings
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('yata.views',
     # Example:
-    (r'^yata/$', 'yata.views.index'),
-    (r'^yata/add_task/$',              'yata.views.edit'),
-    (r'^yata/(?P<task_id>\d+)/edit/$', 'yata.views.edit'),
-    url(r'^yata/(?P<task_id>\d+)/mark_done/$', 'yata.views.mark_done', name='mark-task-done'),
-    url(r'^yata/(?P<task_id>\d+)/mark_not_done/$', 'yata.views.mark_done', {'b': False}, name='mark-task-not-done'),
-    (r'^yata/context/show/(?P<context_id>\d+)/$', 'yata.views.select_context'),
-    (r'^yata/context/show/all/$', 'yata.views.select_context_all'),
-    (r'^yata/context/show/none/$', 'yata.views.select_context_none'),
-    (r'^yata/context/add/$', 'yata.views.edit_context'),
-    (r'^yata/context/(?P<id>\d+)/edit/$', 'yata.views.edit_context'),
-    (r'^yata/context/(?P<id>\d+)/delete/$', 'yata.views.delete_context'),
-    (r'^yata/task/(?P<id>\d+)/delete/$', 'yata.views.delete_task'),
-    (r'^yata/xml/import/$', 'yata.views.xml_import'),
-    (r'^yata/xml/export/$', 'yata.views.xml_export'),
-    (r'^yata/future/show/$', 'yata.views.show_future_tasks', {'b': True}),
-    (r'^yata/future/hide/$', 'yata.views.show_future_tasks', {'b': False}),
-    (r'^yata/done/yes/$', 'yata.views.show_tasks_done', {'b': 'Done'}),
-    (r'^yata/done/no/$', 'yata.views.show_tasks_done', {'b': 'Active'}),
-    (r'^yata/done/all/$', 'yata.views.show_tasks_done', {'b': 'All'}),
-    
-    
+    (r'^yata/$', 'index'),
+    (r'^yata/add_task/$',              'edit'),
+    (r'^yata/(?P<task_id>\d+)/edit/$', 'edit'),
+    url(r'^yata/(?P<task_id>\d+)/mark_done/$', 'mark_done', name='mark-task-done'),
+    url(r'^yata/(?P<task_id>\d+)/mark_not_done/$', 'mark_done', {'b': False}, name='mark-task-not-done'),
+    (r'^yata/context/show/(?P<context_id>\d+)/$', 'select_context'),
+    (r'^yata/context/show/all/$', 'select_context_all'),
+    (r'^yata/context/show/none/$', 'select_context_none'),
+    (r'^yata/context/add/$', 'edit_context'),
+    (r'^yata/context/(?P<id>\d+)/edit/$', 'edit_context'),
+    (r'^yata/context/(?P<id>\d+)/delete/$', 'delete_context'),
+    (r'^yata/task/(?P<id>\d+)/delete/$', 'delete_task'),
+    (r'^yata/xml/import/$', 'xml_import'),
+    (r'^yata/xml/export/$', 'xml_export'),
+    (r'^yata/future/show/$', 'show_future_tasks', {'b': True}),
+    (r'^yata/future/hide/$', 'show_future_tasks', {'b': False}),
+    (r'^yata/done/yes/$', 'show_tasks_done', {'b': 'Done'}),
+    (r'^yata/done/no/$', 'show_tasks_done', {'b': 'Active'}),
+    (r'^yata/done/all/$', 'show_tasks_done', {'b': 'All'}),
+)
+
+urlpatterns += patterns('',    
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
-    
+
     (r'^static_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.XNO_LOCAL_PREFIX+'public/static_media/', 'show_indexes': True }),    
 )
