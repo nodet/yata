5bf05613b91619032d289aa6241bbfce644f1681
Xavier Nodet <xavier.nodet@fr.ibm.com>
2013-03-08 22:21:40 +0100

    Let's diff the output of the test.

--- yata2toodledo/run_y2t.sh
+++ yata2toodledo/run_y2t.sh
@@ -1,1 +1,2 @@
-java org.apache.xalan.xslt.Process -IN tests/yata.xml -XSL y2t.xml -out tests/too.xml
+java org.apache.xalan.xslt.Process -IN tests/yata.xml -XSL y2t.xml -out tests/too.out.xml
+diff tests/too.xml tests/too.out.xml
